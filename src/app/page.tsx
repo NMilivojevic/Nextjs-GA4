d236379a8a426ec355e2314c9d1eeb7b0f33c712
NMilivojevic <nevmilivojevic@gmail.com>
2023-07-31 16:50:38 +0200

    add env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import Script from "next/script";
+
+const MEASUREMENT_ID = process.env.MEASUREMENT_ID;
+
 export default function Home() {
-    return <h1>Hello</h1>;
+    return (
+        <div>
+            <h1>Nextjs google analytics 4</h1>
+            <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${MEASUREMENT_ID}`}
+            />
+            <Script id="google-analytics">
+                {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', ${MEASUREMENT_ID});
+        `}
+            </Script>
+        </div>
+    );
 }
